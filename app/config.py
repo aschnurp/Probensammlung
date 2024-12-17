3ac4387eb97d61e2cd22c0aa291cdfeb1552cced
aschnurp <anton24697@gmx.de>
2024-12-17 11:09:31 +0100

    update config and remove unnecessary content

--- app/config.py
+++ app/config.py
@@ -10,29 +10,6 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     frontend_path: str
-    db_connection_type: str = "unix"  # Default to 'unix'
-    algorithm: str
-    secret_key: str
-    access_token_expire_minutes: int
-    forget_password_link_expire_minutes: int
-    next_public_contact_name_one: str
-    next_public_contact_mail_one: str
-    next_public_contact_name_two: str
-    next_public_contact_mail_two: str
-#    secret_key: str
-#    algorithm: str
-#    access_token_expire_minutes: int
-#    forget_password_link_expire_minutes: int
-#    mail_username: str
-#    mail_password: str
-#    mail_from: str
-#    mail_port: int
-#    mail_server: str
-#    mail_from_name: str
-#    mail_starttls: bool
-#    mail_ssl_tls: bool
-#    use_credentials: bool
-#    validate_certs: bool
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
