43a5cafad22b034967ee1f35a9f7a74ca47314c1
aschnurp <anton24697@gmx.de>
2025-11-12 13:16:55 +0100

    change label in patient_anlegen to Patienten ID (intern) z.B. HL1234

--- frontend/src/pages/patient_anlegen.js
+++ frontend/src/pages/patient_anlegen.js
@@ -221,7 +221,7 @@ export default function PatientForm() {
       </Box>
 
       <TextField
-        label="Patienten ID (intern)"
+        label="Patienten ID (intern) z.B. HL1234"
         name="patient_Id_intern"
         value={formData.patient_Id_intern || ''}
         onChange={handleChange}
