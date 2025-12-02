5224a471340158ce513dc3b2b767441e2826f2a4
aschnurp <anton24697@gmx.de>
2025-12-02 10:39:49 +0100

    add to overviews view

--- frontend/src/types/patientColumns.ts
+++ frontend/src/types/patientColumns.ts
@@ -1,10 +1,11 @@
 export const patientDataColumns = [
     { key: "patient_Id_intern", label: "Patient/Innen ID (intern)" },
     { key: "sap_id", label: "SAP ID" },
+    { key: "sap_fallnummer", label: "SAP Fallnummer" },
     { key: "created_at", label: "Erstellungsdatum" },
     { key: "geschlecht", label: "Geschlecht" },
     { key: "alter", label: "Alter" },
     { key: "op_diagnose", label: "OP Diagnose" },
     { key: "op_geplant", label: "Geplante OP"},
-    { key: "bemerkung", label: "SAP Fallnummer / Bemerkungen" }
+    { key: "bemerkung", label: "Bemerkungen" }
 ];
